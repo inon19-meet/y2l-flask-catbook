8e4638b2cb63ad9abfdfd355a4c1ffd2b5f991a8
= <=>
2018-11-22 18:40:55 +0200

    k

--- app.py
+++ app.py
@@ -27,7 +27,14 @@ def homepage():
     if request.method == 'GET':
         return render_template("addcat.html")
     else:
-        return render_template("addcat.html")
+    	create_cat(request.form['firstname'])
+    	return render_template("new_cat.html")
+
+
+@app.route('/new_cat')
+def newcat():
+	new_cat = create_cat()
+	return render_template("new_cat.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
